5854c18f6bce79ff4bab90461deb21d005c6c3a8
Gedankenleid <Gedankenleid@users.noreply.github.com>
2020-05-17 17:51:07 +0200

    Initial commit

---
+++ src/main/java/dev/larsii/chatmention/ChatMention.java
@@ -0,0 +1,24 @@
+package dev.larsii.chatmention;
+
+import net.labymod.api.LabyModAddon;
+import net.labymod.settings.elements.SettingsElement;
+
+import java.util.List;
+
+public class ChatMention extends LabyModAddon {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void loadConfig() {
+
+    }
+
+    @Override
+    protected void fillSettings(List<SettingsElement> list) {
+
+    }
+}
